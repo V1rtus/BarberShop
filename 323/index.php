e95a0c15d41acc3f7c22854582f861120e2ac722
Viktor Dotsenko <kh93bi@gmail.com>
2016-04-07 22:36:21 +0300

    re

---
+++ 323/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+    <title>BarberShop</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+  <link href="https://fonts.googleapis.com/css?family=Merriweather:400,700,900,300&subset=latin,cyrillic" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Russo+One&subset=latin,cyrillic" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+  <!-- header -->
+  <header>
+    <nav>
+      <ul class="menu">
+        <li>Информация</li>
+        <li>Новости</li>
+        <li>Прайс-лист</li>
+        <li>Магазин</li>
+        <li>Контакты</li>
+        <li>Вход</li>
+      </ul>
+    </nav>
+  </header>
+  <!-- header -->
+
+  <!-- main -->
+  <div class="logo">Логотип</div>
+    <h1>Содержание</h1>
+      <p>Первый макет сайта по интенсиву от htmlacademy. Выбор макета еще не сделан, но это основной для тестирования навыков.</p>
+
+  <div class="map">
+    <h2>23</h2>
+      <p>23434</p>
+  </div>
+
+  <div class="typograph">
+    <p>Простой текст показывающий различные стайлгайды <strong>усиление семантическое</strong> и после пойдет выделение жирным <b>без семантической важнойсти</b>.</p>
+    <p>Тоже самое можно сказать и о <em>курсе с важностью </em> и после показать нужно обязательно <i>без важности</i></p>
+    <p>Очень интересный пример <mark>метки</mark></p>
+  
+  </div>
+  <!-- main -->
+
+  <!-- footer -->
+  <!-- footer -->
+  </body>
+</html>
